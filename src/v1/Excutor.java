e4bdd822423ff1f45bde04df4b272d026331c729
LINSONGJIN <1124173420@qq.com>
2019-02-26 08:32:14 +0800

    deleta Excutor.java file

--- src/v1/Excutor.java
+++
@@ -1,9 +0,0 @@
-package v1;
-
-import v1.config.MapperRegistory;
-
-import java.sql.SQLException;
-
-public interface Excutor {
-    <T> T query(MapperRegistory.MapperData statem, Object parameter) throws SQLException;
-}
